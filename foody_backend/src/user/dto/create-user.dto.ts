e17436328d9046e9b2c828dbb4eed4c37e41d238
nadajarboui99 <124178994+nadajarboui99@users.noreply.github.com>
2025-01-27 22:34:24 +0100

    dto for create and update user

--- foody_backend/src/user/dto/create-user.dto.ts
+++ foody_backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsNotEmpty, MinLength } from 'class-validator';
+
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(8, { message: 'Le mot de passe doit contenir au moins 8 caractères.' })
+  password: string;
+
+}
--- foody_backend/src/user/dto/update-user.dto.ts
+++ foody_backend/src/user/dto/update-user.dto.ts
@@ -1,4 +1,15 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString, MinLength, IsEmail } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  username?: string;
+
+  @IsOptional()
+  @MinLength(8, { message: 'Le mot de passe doit contenir au moins 8 caractères.' })
+  password?: string;
+
+  @IsOptional()
+  @IsEmail({}, { message: 'Veuillez fournir une adresse email valide.' })
+  email?: string;
+}
