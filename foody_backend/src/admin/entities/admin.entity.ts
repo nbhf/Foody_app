1659dd70cc3d504b6127ef315092f75ef1aacfac
Shams Ben Mefteh <shams.benmefteh@insat.ucar.tn>
2025-01-28 11:26:07 +0100

    fixed compilation erroes

--- foody_backend/src/admin/entities/admin.entity.ts
+++ foody_backend/src/admin/entities/admin.entity.ts
@@ -1,10 +1,19 @@
 import { Recipe } from "src/recipe/entities/recipe.entity";
-import { Column, Entity, OneToMany } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "src/user/entities/user.entity";
 import { UserRoleEnum } from "src/user/enums/user-role.enum";
 
 @Entity('admin')
-export class Admin extends User {
+export class Admin {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+      length: 50,
+      unique: true
+    })
+    username: string
 
     @Column({
         type: 'enum',
--- foody_backend/src/recipe/recipe.module.ts
+++ foody_backend/src/recipe/recipe.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RecipeService } from './recipe.service';
 import { RecipeController } from './recipe.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Recipe } from './entities/recipe.entity';
 
 @Module({
   controllers: [RecipeController],
   providers: [RecipeService],
+  imports: [
+    TypeOrmModule.forFeature([Recipe])
+  ]
 })
 export class RecipeModule {}
