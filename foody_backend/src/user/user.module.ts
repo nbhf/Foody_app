c1d00151cb758aee5f88e6bc4660340c02fea68e
Shams <79046651+shamss778@users.noreply.github.com>
2025-01-28 11:13:12 +0100

    Update user.module.ts

--- foody_backend/src/user/user.module.ts
+++ foody_backend/src/user/user.module.ts
@@ -1,12 +1,12 @@
 // src/modules/user/user.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './entities/user.entity';
+import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])], // Enregistre l'entité avec TypeORM
+  imports: [TypeOrmModule.forFeature([User])], // Enregistre l'entité avec TypeORM
   providers: [UserService],
   controllers: [UserController],
   exports: [TypeOrmModule], // Exporte l'entité pour que d'autres modules puissent l'utiliser
