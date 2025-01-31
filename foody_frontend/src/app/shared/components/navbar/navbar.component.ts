12bdbe59b8efea30e9190e9ef562a30b96e0a798
nbhf <houdabhf123@gmail.com>
2025-02-01 00:38:49 +0100

    username showing in navbar and logout working

--- foody_frontend/src/app/shared/components/navbar/navbar.component.ts
+++ foody_frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  username: string | null = null;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.username = this.authService.getUser(); 
+  }
+
+  logout() {
+    this.authService.logout();
+    this.username = null; 
+  }
 }
