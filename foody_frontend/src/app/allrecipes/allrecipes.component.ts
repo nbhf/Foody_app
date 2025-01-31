4a73cd9d070ae005e0a904dd1d01f3bc39086515
nbhf <houdabhf123@gmail.com>
2025-01-31 21:13:17 +0100

    polling recipes evry 5 seconds

--- foody_frontend/src/app/allrecipes/allrecipes.component.ts
+++ foody_frontend/src/app/allrecipes/allrecipes.component.ts
@@ -1,25 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AllRecipeService } from './allrecipes.service';
 import { Recipe } from '../shared/models/recipe.model';
+import { interval } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-allrecipes',
   templateUrl: './allrecipes.component.html',
   styleUrls: ['./allrecipes.component.css']
 })
-export class AllrecipesComponent {
+export class AllrecipesComponent implements OnInit {
   recipes: Recipe[] = [];
+  private readonly pollInterval = 5000; // Polling every 5 seconds
 
   constructor(private recipeService: AllRecipeService) {}
 
   ngOnInit() {
-    this.getRecipes();
-  }
-
-  getRecipes() {
-    this.recipeService.getRecipes().subscribe(
-      (data) => this.recipes = data,
-      (error) => console.error('Erreur lors de la récupération des recettes', error)
-    );
+    interval(this.pollInterval)
+      .pipe(switchMap(() => this.recipeService.getRecipes())) 
+      .subscribe(
+        (data) => this.recipes = data,
+        (error) => console.error('Erreur lors de la récupération des recettes', error)
+      );
   }
 }
