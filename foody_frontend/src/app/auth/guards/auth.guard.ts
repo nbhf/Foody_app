e65072aa2422597f3d8b7716a91dd7165d8fc2c1
nbhf <houdabhf123@gmail.com>
2025-01-31 19:50:02 +0100

    apis config and redirection apres guard et login

--- foody_frontend/src/app/auth/guards/auth.guard.ts
+++ foody_frontend/src/app/auth/guards/auth.guard.ts
@@ -4,9 +4,9 @@ import { APP_ROUTES } from 'src/app/config/app-routes.config';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('access_token');
   if (!token) {
-    router.navigate([APP_ROUTES.home]);
+    router.navigate([APP_ROUTES.login], { queryParams: { returnUrl: state.url } });
     return false;
   }
   return true;
---
+++ foody_frontend/src/app/config/app-api.config.ts
@@ -0,0 +1,10 @@
+export const APP_API = {
+    login: 'http://localhost:3000/auth/login',
+    signup: 'http://localhost:3000/auth/signup',
+    user: 'http://localhost:3000/user',
+    admin:'http://localhost:3000/admin',
+    auth: 'http://localhost:3000/auth',
+    recipe:'http://localhost:3000/recipe',
+    comment:'http://localhost:3000/comments',
+  
+  }
