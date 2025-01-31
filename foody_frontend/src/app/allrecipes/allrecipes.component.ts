de63161483d74f3a456babe59fae2c24b73ab000
nbhf <houdabhf123@gmail.com>
2025-01-31 21:27:09 +0100

    smooth transition and 30s poll

--- foody_frontend/src/app/allrecipes/allrecipes.component.ts
+++ foody_frontend/src/app/allrecipes/allrecipes.component.ts
@@ -11,11 +11,18 @@ import { switchMap } from 'rxjs/operators';
 })
 export class AllrecipesComponent implements OnInit {
   recipes: Recipe[] = [];
-  private readonly pollInterval = 5000; // Polling every 5 seconds
+  private readonly pollInterval = 30000; // Polling every 5 seconds
 
   constructor(private recipeService: AllRecipeService) {}
 
   ngOnInit() {
+    // Immediately fetch the data when the component is loaded
+    this.recipeService.getRecipes().subscribe(
+      (data) => this.recipes = data,
+      (error) => console.error('Erreur lors de la récupération des recettes', error)
+    );
+
+    // Start polling after the initial fetch
     interval(this.pollInterval)
       .pipe(switchMap(() => this.recipeService.getRecipes())) 
       .subscribe(
